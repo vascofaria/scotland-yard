64924543599cccbfb3f45ea98e993e5dc80c023b
Vasco Faria <vascoffaria@gmail.com>
2019-10-24 22:29:16 +0100

    BFS for 3

--- ruagomesfreiregamesol.py
+++ ruagomesfreiregamesol.py
@@ -1,6 +1,7 @@
 import math
 import pickle
 import time
+import heapq, time, sys
 
 def minIndex(l):
   _min = math.inf
@@ -25,38 +26,139 @@ class SearchProblem:
     self.auxheur = auxheur
 
     self.positions   = [0]
-    self.agentStatus = []
+    self.heapSet = [[], [], []]
+    self.agentStatus = [False, False, False]
 
   def search(self, init, limitexp = 2000, limitdepth = 10, tickets = [math.inf,math.inf,math.inf], anyorder = False):
     path = []
     self.tickets = tickets
 
     if (len(init) == 1):
-      path.append([[], [init[0]]])
-      position = init[0]
+      return self.bfs1(init[0])
+      #path.append([[], [init[0]]])
+      #position = init[0]
 
-      while position != self.goal[0]:
-        transp, position = self.expand(position, 0)
-        self.tickets[transp] -= 1
-        path.append([[transp], [position]])
+      #while position != self.goal[0]:
+      #  transp, position = self.expand(position, 0)
+      #  self.tickets[transp] -= 1
+      #  path.append([[transp], [position]])
 
     else:
+      self.bfs3(init, limitexp)
+      return []
       path.append([[], [init[0], init[1], init[2]]])
       self.positions = [init[0], init[1], init[2]]
 
-      # while (self.positions[0] != self.goal[0]) or (self.positions[1] != self.goal[1]) or (self.positions[2] != self.goal[2]):
-      #   pass
+      # for agentNumber in range(3):
+      #   heapq.heappush(self.heapSet[agentNumber], (math.inf, init[agentNumber], None)) # (h, p, t)
+
+      while self.positions[0] != self.goal[0] or self.positions[1] != self.goal[1] or self.positions[2] != self.goal[2]: #not self.agentStatus[0] or not self.agentStatus[1] or not self.agentStatus[2]:
+        transp0, self.positions[0] = self.expand(self.positions[0], 0)
+        self.tickets[transp0] -= 1
+        transp1, self.positions[1] = self.expand(self.positions[1], 1)
+        self.tickets[transp1] -= 1
+        transp2, self.positions[2] = self.expand(self.positions[2], 2)
+        self.tickets[transp2] -= 1
+
+        path.append([[transp0, transp1, transp2], [self.positions[0], self.positions[1], self.positions[2]]])
+
+        #for agentNumber in range(3):
+        #  current = heapq.heappop(self.heapSet[agentNumber])
+        #  print('Current ' + str(current))
+        #  print('Positions ' + str(self.positions))
+        #  self.positions[agentNumber] = current[1]
+        #  path[len(path)-1][0].append(current[2])
+        #  path[len(path)-1][1].append(current[1])
+        #  self.expand_heap(current[1], agentNumber)
+        #  print(path)
+        # print(f'Agent: {agentNumber} --> {self.heapSet[agentNumber]}')
+
+      print(path)
 
     return path
 
-  def expand(self, position, agent_number):
+  def expand(self, position, agentNumber):
     childs = [child[1] for child in self.model[position]]
-    heuristics = [self.heuristicFunction(position, child, agent_number) for child in childs]
+    heuristics = [self.heuristicFunction(position, child, agentNumber) for child in childs]
     return self.model[position][minIndex(heuristics)][0], childs[minIndex(heuristics)]
+
+  def expand_heap(self, position, agentNumber):
+    for route in self.model[position]:
+      if (self.heuristicFunction(position, route[1], agentNumber)) != math.inf:
+        heapq.heappush(self.heapSet[agentNumber], (self.heuristicFunction(position, route[1], agentNumber), route[1], route[0]))
+
+  def expand_child(self, position):
+    for route in self.model[position]:
+      self.checkRoute(route, agentNumber)
+
+  def checkRoute(self, route, agentNumber):
+    if route[1] == self.goal[agentNumber]:
+      self.agentStatus[agentNumber] = True
+
+  def bfs3(self, init, limitexp):
+    queue = []
+    currents = [None, None, None]
+
+    done = False
+
+    queue.append([(init[0], None, None)])
+    queue.append([(init[1], None, None)])
+    queue.append([(init[2], None, None)])
+
+    while not done and limitexp > 0:
+      if currents[0] != None and currents[1] != None and currents[2] != None:
+        if currents[0][0] == self.goal[0] and currents[1][0] == self.goal[1] and currents[2][0] == self.goal[2]:
+          # checkar tickets and same position
+          for agentNumber in range(3):
+            aux = currents[agentNumber]
+            while aux[1] != None:
+              print(aux[0])
+              aux = aux[1]
+          done = True
+        
+        print(currents[0][0], currents[1][0], currents[2][0])
+
+      for agentNumber in range(3):
+        currents[agentNumber] = queue[agentNumber].pop(0)
+
+        for vertex in self.model[currents[agentNumber][0]]:
+          queue[agentNumber].append((vertex[1], currents[agentNumber], vertex[0]))
+
+      limitexp -= 1
+
+  def bfs1(self, srcVertex):
+    queue = []
+    path = []
+
+    queue.append((srcVertex, None, None, self.tickets[:]))
+
+    done = False
+
+    while len(queue) != 0 and not done:
+      current = queue.pop(0)
+
+      if current[0] == self.goal[0]:
+        aux = current
+        while aux[1] != None:
+          path.insert(0, [[aux[2]], [aux[0]]])
+          print(aux[0])
+          aux = aux[1]
+          done = True
+
+      for vertex in self.model[current[0]]:
+        ticketsAux = current[3][:]
+        if ticketsAux[vertex[0]] > 0 and vertex[1] != current[1]:
+          ticketsAux[vertex[0]] -= 1
+          queue.append((vertex[1], current, vertex[0], ticketsAux))
+          self.tickets[vertex[0]] -= 1
+
+    path.insert(0, [[], [srcVertex]])
+    return path
+
 
   def heuristicFunction(self, srcVertex, destVertex, agent_number):
     objVertex = self.goal[agent_number]
-    if not isPositionFree(destVertex, self.positions[:agent_number]):
+    if not isPositionFree(destVertex, self.positions):
       return math.inf
     if self.tickets[self.getTicketType(srcVertex, destVertex)] > 0:
       return math.sqrt(math.pow(self.getCoordinates(objVertex)[0] - self.getCoordinates(destVertex)[0], 2) + math.pow(self.getCoordinates(objVertex)[1] - self.getCoordinates(destVertex)[1], 2))
